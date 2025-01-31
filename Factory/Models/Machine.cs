9fbeafc934da25d5b0cef751224cb989fc070d9c
igl-myrick <igl.myrick@gmail.com>
2025-01-31 14:04:35 -0800

    add Machine class

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    [Required(ErrorMessage = "Please enter a valid name.")]
+    public int Name { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
