bbc0bd28d7caff134edf5e5b2f42f5cc059d3405
igl-myrick <igl.myrick@gmail.com>
2025-02-04 13:30:26 -0800

    add DeleteJoin route

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -91,5 +91,14 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachines.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
