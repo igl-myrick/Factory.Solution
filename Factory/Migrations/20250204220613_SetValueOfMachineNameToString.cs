b8ea7a764c8257c7e775f249221bee69b39633cb
igl-myrick <igl.myrick@gmail.com>
2025-02-04 14:53:48 -0800

    set value of machine name to string

---
+++ Factory/Migrations/20250204220613_SetValueOfMachineNameToString.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class SetValueOfMachineNameToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Machines",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
